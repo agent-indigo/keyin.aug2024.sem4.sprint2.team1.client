af0f1d14c1bd87369deda32ad98aad5a3b3dee18
agent-indigo <bradenhynes@gmail.com>
2024-07-10 14:27:19 -0230

    Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import {BrowserRouter} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import {ToastContainer} from 'react-toastify'
+import store from './store'
+import 'react-toastify/dist/ReactToastify.css'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Provider store={store}>
+      <BrowserRouter>
+        <ToastContainer/>
+      </BrowserRouter>
+    </Provider>
+  )
 }
-
-export default App;
+export default App
