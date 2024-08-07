07fa7a3236f4b53335adba6914b3ded80c426d2d
ashley-colbert <136830986+ashley-colbert@users.noreply.github.com>
2024-08-07 10:46:07 -0230

    signup with axios

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,109 +1,108 @@
-import {useAddMutation} from '../slices/contactsApiSlice'
-import {useState} from 'react'
-import {Link, useNavigate} from 'react-router-dom'
+import axios from "axios";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
 const Signup = () => {
   const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    phoneNumber: '',
-    streetAddress: '',
-    city: '',
-    province: '',
-    postalCode: '',
-    password: '',
-    confirmPassword: '',
-  })
+    first: "",
+    last: "",
+    active: true,
+    role: "customer",
+    address: "",
+    number: "",
+    street: "",
+    city: "",
+    prov: "",
+    postal: "",
+    category: "customer",
+    // password: '',
+    // confirmPassword: '',
+  });
   const navigate = useNavigate();
-  const [addContact, {isLoading, isError, error}] = useAddMutation();
-  const handleChange = event => {
-    const {id, value} = event.target
-    setFormData(prevState => ({
+
+  const handleChange = (event) => {
+    const { id, value } = event.target;
+    setFormData((prevState) => ({
       ...prevState,
       [id]: value,
-    }))
-  }
-  const handleSubmit = event => {
-    event.preventDefault()
-    // Add logic for handling sign up here
-    const response = addContact(formData).unwrap()
-    console.log('Form Data:', response)
-    // Assuming the sign-up process is successful
-    navigate('/my-account')
-  }
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      console.log("Form Data before submission:", formData);
+  
+      const contactResponse = await axios.post("/contacts", {
+        first: formData.first,
+        last: formData.last,
+        active: formData.active,
+        role: formData.role,
+      });
+  
+      //I want this to retrieve the contact fk to put it in the respective tables, but it doesn't seem to be working
+      const contactId = contactResponse.data.fk;
+  
+      const emailResponse = await axios.post("/api/emails", {
+        address: formData.address,
+        category: formData.category,
+        active: formData.active,
+        contactId: contactId,
+      });
+  
+      const phoneResponse = await axios.post("/api/phones", {
+        number: formData.number,
+        category: formData.category,
+        active: formData.active,
+        contactId: contactId,
+      });
+  
+      const addressResponse = await axios.post("/api/addresses", {
+        street: formData.street,
+        city: formData.city,
+        prov: formData.prov,
+        postal: formData.postal,
+        active: formData.active,
+        category: formData.category,
+        contactId: contactId,
+      });
+  
+      console.log("Form Data after submission:", {
+        contact: contactResponse.data,
+        email: emailResponse.data,
+        phone: phoneResponse.data,
+        address: addressResponse.data,
+      });
+      navigate("/my-account");
+    } catch (err) {
+      console.error("Failed to sign up:", err);
+    }
+  };
+
   return (
-    <div className='flex justify-center items-center min-h-screen bg-[#D8D7D7]'>
-      <div className='w-full max-w-2xl p-10 pt-16 space-y-10 bg-[#D8D7D7]'>
-        <div className='space-y-0'>
-          <h2 className='text-4xl text-[#040200] text-center'>
-            SIGN UP
-          </h2>
-          <p className='text-md text-[#040200] text-center'>
+    <div className="flex justify-center items-center min-h-screen bg-[#D8D7D7]">
+      <div className="w-full max-w-2xl p-10 pt-16 space-y-10 bg-[#D8D7D7]">
+        <div className="space-y-0">
+          <h2 className="text-4xl text-[#040200] text-center">SIGN UP</h2>
+          <p className="text-md text-[#040200] text-center">
             Please complete all fields below to create your account
           </p>
         </div>
-        <form
-          className='space-y-6'
-          onSubmit={handleSubmit}
-        >
+        <form className="space-y-6" onSubmit={handleSubmit}>
           {[
-            {
-              id: 'firstName',
-              label: 'First Name'
-            },
-            {
-              id: 'lastName',
-              label: 'Last Name'
-            },
-            {
-              id: 'email',
-              label: 'Email',
-              type: 'email'
-            },
-            {
-              id: 'phoneNumber',
-              label: 'Phone Number',
-              type: 'tel'
-            },
-            {
-              id: 'streetAddress',
-              label: 'Street Address'
-            },
-            {
-              id: 'city',
-              label: 'City'
-            },
-            {
-              id: 'province',
-              label: 'Province'
-            },
-            {
-              id: 'postalCode',
-              label: 'Postal Code'
-            },
-            {
-              id: 'password',
-              label: 'Password',
-              type: 'password'
-            },
-            {
-              id: 'confirmPassword',
-              label: 'Confirm Password',
-              type: 'password'
-            },
-          ].map(({
-            id,
-            label,
-            type = 'text'
-          }) => (
-            <div
-              key={id}
-              className='flex items-center'
-            >
-              <label
-                htmlFor={id}
-                className='w-1/3 text-xl text-[#040200]'
-              >
+            { id: "first", label: "First Name" },
+            { id: "last", label: "Last Name" },
+            { id: "address", label: "Email", type: "email" },
+            { id: "number", label: "Phone Number", type: "tel" },
+            { id: "street", label: "Street Address" },
+            { id: "city", label: "City" },
+            { id: "prov", label: "Province" },
+            { id: "postal", label: "Postal Code" },
+            // { id: 'password', label: 'Password', type: 'password' },
+            // { id: 'confirmPassword', label: 'Confirm Password', type: 'password' },
+          ].map(({ id, label, type = "text" }) => (
+            <div key={id} className="flex items-center">
+              <label htmlFor={id} className="w-1/3 text-xl text-[#040200]">
                 {label}
               </label>
               <input
@@ -111,35 +110,26 @@ const Signup = () => {
                 id={id}
                 value={formData[id]}
                 onChange={handleChange}
-                className='w-2/3 p-1 ml-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-neutral-400'
+                className="w-2/3 p-1 ml-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-neutral-400"
                 required
               />
             </div>
           ))}
           <button
-            type='submit'
-            className='w-full p-2 text-xl text-[#5595AC] bg-[#040200] rounded focus:outline-none focus:ring-2 focus:ring-neutral-400'
-            disabled={isLoading}
+            type="submit"
+            className="w-full p-2 text-xl text-[#5595AC] bg-[#040200] rounded focus:outline-none focus:ring-2 focus:ring-neutral-400"
           >
-            {isLoading ? 'Signing Up...' : 'Sign Up'}
+            Sign Up
           </button>
-          {isError && (
-            <p className='text-red-500'>
-              Error: {error?.data?.message || 'Failed to sign up'}
-            </p>
-           )}
         </form>
-        <p className='text-md text-center text-gray-600'>
-          Already have an account?{' '}
-          <Link
-            to='/login'
-            className='text-[#5595AC] hover:underline'
-          >
-            Log in here!
-          </Link>.
+        <p className="text-md text-center text-gray-600">
+          Already have an account?{" "}
+          <Link to="/login" className="text-[#5595AC] hover:underline">
+            Log in here
+          </Link>
         </p>
       </div>
     </div>
-  )
-}
-export default Signup
+  );
+};
+export default Signup;
